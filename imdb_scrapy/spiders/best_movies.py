4c9236179a4eeabe01c0d96fde1de743d4e00a96
root <root@localhost.localdomain>
2020-04-26 10:53:30 +0530

    first page get all info

---
+++ imdb_scrapy/spiders/best_movies.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class BestMoviesSpider(CrawlSpider):
+    name = 'best_movies'
+    allowed_domains = ['imdb.com']
+    start_urls = ['https://www.imdb.com/search/title/?groups=top_250&sort=user_rating']
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']/a"), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        yield{
+            'title':response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'year':response.xpath("//span[@id='titleYear']/a/text()").get(),
+            'duration':response.xpath("normalize-space((//time)[1]/text())").get(),
+            'genere':response.xpath("//div[@class='subtext']/a[1]/text()").get(),
+            'rating':response.xpath("//span[@itemprop='ratingValue']/text()").get(),
+            'movie_url':response.url,
+        }
